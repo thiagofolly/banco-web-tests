e61c5c1e09adf53433e3f7976362ca304f8c9dc0
Thiago Folly de Oliveira <folly@MacBook-Pro-de-Thiago.local>
2026-08-02 17:19:29 -0300

    removendo .only esquecido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,7 +3,7 @@ describe('login', () => {
     cy.visit('http://localhost:4000')
     cy.screenshot('apos-visitar-pagina')
   })
-  it.only('login com dados validos deve permitir entrada no sistema', () => {
+  it('login com dados validos deve permitir entrada no sistema', () => {
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
     cy.screenshot('apos-preencher-dados-validos')
