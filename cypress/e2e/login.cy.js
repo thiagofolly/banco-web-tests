aacddc16ca503e3607117430855842fdf73e7451
Thiago Folly de Oliveira <folly@MacBook-Pro-de-Thiago.local>
2026-08-02 16:39:18 -0300

    adicionei o beforeEach para acesso a pagina de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,16 @@
 describe('login', () => {
-  it('login com dados validos deve permitir entrada no sistema', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:4000')
+  })
+  it('login com dados validos deve permitir entrada no sistema', () => {
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
     cy.contains('button', 'Entrar').click()
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
-  
+
   it('login com dados invalidos deve apresentar mensagem de erro', () => {
-    cy.visit('http://localhost:4000')
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('654321')
     cy.contains('button', 'Entrar').click()
