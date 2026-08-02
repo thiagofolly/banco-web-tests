10208b4bf815fb75a0fe7a852a15bc774dbca4b5
Thiago Folly de Oliveira <folly@MacBook-Pro-de-Thiago.local>
2026-08-02 15:43:41 -0300

    adicionei teste para validar login com credenciais invalidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,8 +3,17 @@ describe('login', () => {
     cy.visit('http://localhost:4000')
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+  
+  it('login com dados invalidos deve apresentar mensagem de erro', () => {
+    cy.visit('http://localhost:4000')
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('654321')
+    cy.contains('button', 'Entrar').click()
+
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
 })
